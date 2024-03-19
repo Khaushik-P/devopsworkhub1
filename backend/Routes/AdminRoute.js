dcdc0646d49fa8335290156bb5348056b389cf32
Khaushik-P <khaushik2004@gmail.com>
2024-03-19 18:28:53 +0530

    Modifications

--- backend/Routes/AdminRoute.js
+++ backend/Routes/AdminRoute.js
@@ -7,8 +7,7 @@ import path from "path";
 
 const router = express.Router();
 
-router.post("/adminlogin", (req, res) => {
-    console.log(req.body)   
+router.post("/adminlogin", (req, res) => { 
   const sql = "SELECT * from admin Where email = ? and password = ?";
   con.query(sql, [req.body.email, req.body.password], (err, result) => {
     if (err) return res.json({ loginStatus: false, Error: "Query error" });
